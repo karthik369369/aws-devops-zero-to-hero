5a167107beb1adf3da540ac0ce6af580ac48ff60
karthik369369 <karthikankiraopally369@gmail.com>
2025-05-19 13:15:50 +0530

    update

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull karthikf5/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 simple-python-flask-app/simple-python-flask-app
+docker run -d -p 5000:5000 karthikf5/simple-python-flask-app/simple-python-flask-app
